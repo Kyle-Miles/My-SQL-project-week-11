f5a97efc11f8f23051f5dc3dcd03faad0d520045
KYLE Miles (19605033) <kmiles123@email.itt-tech.edu>
2022-11-19 17:12:14 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My-SQL project week 11
---
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,45 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+import projects.exception.DbException;
+
+public class ProjectService {
+	private ProjectDao projectDao = new ProjectDao();
+
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects()
+				.stream()
+				.sorted((r1, r2) -> r1.getProjectId() - r2.getProjectId())
+				.collect(Collectors.toList());
+	}
+
+	
+	public Project fetchProjectbyId(Integer projectId) {
+		return projectDao.fetchProjectById(projectId)
+				.orElseThrow(() -> new NoSuchElementException(
+				"Project with ID ="+ projectId + " does not exist."));
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+		
+	}
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+		}
+	}
+
+}
